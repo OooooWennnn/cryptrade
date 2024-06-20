82af40eb3ebe3706cbb7cbce9d16c69a4039191e
Sumin Han <shan94@myseneca.ca>
2024-06-20 03:13:23 -0400

    Update crypto logo files

--- src/features/premium/CoinLogo.jsx
+++ src/features/premium/CoinLogo.jsx
@@ -1,11 +1,9 @@
 function CoinLogo({ coin }) {
+  console.log(coin.toLowerCase());
+  const ticker = coin.toLowerCase();
   return (
     <div className="flex flex-col items-center justify-between">
-      <img
-        src={`./node_modules/cryptocurrency-icons/svg/color/${coin}.svg`}
-        alt={coin}
-        className="w-10"
-      />
+      <img src={`/icons/svg/color/${ticker}.svg`} alt={coin} className="w-10" />
       <h1 className="font-medium">{coin}</h1>
     </div>
   );
